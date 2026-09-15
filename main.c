0e777e47562683546c0987864d0cc3f9431d35bd
Jamile Oliveira <jamilesiolive@gmail.com>
2026-09-15 11:28:24 -0300

    Módulo geral, Módulo aluno com funções

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #define TamanhoAlunos 3
+
+#define CADASTRO_SUCESSO -1
+#define NAO_LOCALIZADO -2
+#define LOCAL_SUCESSO -3
+#define EXCLUIDO_SUCESSO -4
 
 typedef struct {
     int matricula;
@@ -9,15 +17,17 @@ typedef struct {
     int diaNascimento;
     int mesNascimento;
     int anoNascimento;
-    int cpf;
+    char cpf[12];
 
 } cadastroAlunos;  
 
 	//	protótipo das funções
     int cadastroGeral();
     int menuAlunos();
-    void cadastrarAlunos (cadastroAlunos listaAlunos[], int qtdAlunos);
-	void listarAluno(cadastroAlunos listaAlunos[], int qtdAlunos);
+    int cadastrarAlunos (cadastroAlunos listaAlunos[], int qtdAlunos);
+	void listarAluno (cadastroAlunos listaAlunos[], int qtdAlunos);
+	int atualizarAluno (cadastroAlunos listaAlunos[], int qtdAlunos);
+    int excluirAluno(cadastroAlunos listaAlunos[], int qtdAlunos);
 	
 int main(){
     	
@@ -50,21 +60,55 @@ int main(){
                 sairAluno = 1;
                 break;
             	}
+
 				case 1 :{
-				cadastrarAlunos (listaAlunos, qtdAlunos);
+				int cadastro = cadastrarAlunos (listaAlunos, qtdAlunos);
+                if(cadastro == CADASTRO_SUCESSO){
                 qtdAlunos++;
+                printf("\nAluno cadastrado com sucesso!\n");
+                }
+
         		break;
             	}
+
             	case 2 :{
-				listarAluno(listaAlunos, qtdAlunos);
+				listarAluno (listaAlunos, qtdAlunos);
         		break;
             	}
+
+				case 3:{
 					
-			}	
-		}
+				int localizado = atualizarAluno (listaAlunos, qtdAlunos);
 				
-                break;
-            }
+				if (localizado == NAO_LOCALIZADO) {
+					printf("\nAluno não econtrado!\n");
+				} else if(localizado == LOCAL_SUCESSO) {
+					printf("Aluno atualizado com sucesso!\n ");
+				}
+				    
+				break;
+				}
+
+				case 4:{
+				 
+                 int excluido = excluirAluno(listaAlunos, qtdAlunos);
+
+                  if (excluido == EXCLUIDO_SUCESSO) {
+                    printf("Aluno excluido com sucesso!\n");
+                    qtdAlunos--;
+                  } 
+
+                  break;
+                  }
+			
+				}          	
+					
+			}
+
+            break;	
+		}
+                
+            
             case 2 :{
                 printf("\nProfessores\n");
                 break;
@@ -114,7 +158,7 @@ int main(){
         return opcaoAluno;
  }
  
- void cadastrarAlunos (cadastroAlunos listaAlunos[], int qtdAlunos){
+ int cadastrarAlunos (cadastroAlunos listaAlunos[], int qtdAlunos){
  	
 	  	printf("Digite a matricula: \n");
 	    scanf("%d",&listaAlunos[qtdAlunos].matricula);
@@ -138,9 +182,9 @@ int main(){
 							&listaAlunos[qtdAlunos].anoNascimento);
         
         printf("Digite o CPF (somente numeros): \n");
-        scanf("%d", &listaAlunos[qtdAlunos].cpf);
+        scanf("%s", listaAlunos[qtdAlunos].cpf);
        
-
+            return CADASTRO_SUCESSO;
   }
   
   void listarAluno(cadastroAlunos listaAlunos[], int qtdAlunos){
@@ -152,7 +196,89 @@ int main(){
     printf("Data de nascimento: %d/%d/%d\n", listaAlunos[i].diaNascimento, 
 											 listaAlunos[i].mesNascimento, 
 											 listaAlunos[i].anoNascimento);
-    printf("CPF: %d\n", listaAlunos[i].cpf);
+    printf("CPF: %s\n", listaAlunos[i].cpf);
     }
     
   }
+  
+  int atualizarAluno (cadastroAlunos listaAlunos[], int qtdAlunos){
+  		printf("\nAtualizar aluno\n");
+				
+				listarAluno(listaAlunos, qtdAlunos);
+				
+				    int matricula;
+				    
+				    printf("\nDigite a matricula do aluno: ");
+				    scanf("%d", &matricula);
+				    
+				    int localizado = 0;
+				    
+				    for(int i = 0; i < qtdAlunos; i++){
+				    	
+				    	if (matricula == listaAlunos[i].matricula){
+				    			printf("Digite a matricula: \n");
+				    scanf("%d",&listaAlunos[i].matricula);
+				    getchar();
+				    
+			        printf("Digite o nome: ");
+			        fgets(listaAlunos[i].nome, 50, stdin);
+			        int tam = 0;
+			        while (listaAlunos[i].nome[tam] != '\0'){
+			        	tam++;
+					} if (tam > 0 && listaAlunos[i].nome[tam - 1] == '\n'){
+						listaAlunos[i].nome[tam - 1] = '\0';
+					}
+					
+			        printf("Digite o sexo (F/M): ");
+			        scanf(" %c", &listaAlunos[i].sexo);
+			        
+			        printf("Digite a data de nascimento DD/MM/AAAA: ");
+			        scanf("%d/%d/%d",	&listaAlunos[i].diaNascimento, 
+										&listaAlunos[i].mesNascimento, 
+										&listaAlunos[i].anoNascimento);
+			        
+			        printf("Digite o CPF (somente numeros): ");
+			        scanf("%s", listaAlunos[i].cpf);
+			        
+			        localizado = 1;
+			    	break;
+						}						
+					}
+					if (!localizado){
+						return NAO_LOCALIZADO;
+					} else {
+						return LOCAL_SUCESSO;
+					}
+  }
+  int excluirAluno(cadastroAlunos listaAlunos[], int qtdAlunos){
+
+    printf("\nExcluir aluno\n");
+
+    int matricula;
+    int localizado = 0;
+
+    printf("Digite a matricula do aluno: ");
+    scanf("%d", &matricula);
+
+    for (int i = 0; i < qtdAlunos; i++){
+
+        if (matricula == listaAlunos[i].matricula){
+            for (int j = i; j < qtdAlunos - 1; j++){
+                listaAlunos[j].matricula = listaAlunos[j + 1].matricula;
+                strcpy(listaAlunos[j].nome, listaAlunos[j + 1].nome);
+                listaAlunos[j].sexo = listaAlunos[j + 1].sexo;
+                listaAlunos[j].diaNascimento = listaAlunos[j + 1].diaNascimento;
+                listaAlunos[j].mesNascimento = listaAlunos[j + 1].mesNascimento;
+                listaAlunos[j].anoNascimento = listaAlunos[j + 1].anoNascimento;
+                strcpy(listaAlunos[j].cpf, listaAlunos[j + 1].cpf);
+            }
+            localizado = 1;
+            break;
+        }
+    }
+    if (localizado){
+        return EXCLUIDO_SUCESSO;
+    } else {
+        return NAO_LOCALIZADO;
+    }
+}
